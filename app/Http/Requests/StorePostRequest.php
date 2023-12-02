59d19665d114a5c88faf2d930662f0f5065c5ddc
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2023-12-02 18:21:52 +0400

    Add global size validation on all attachments

--- app/Http/Requests/StorePostRequest.php
+++ app/Http/Requests/StorePostRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Validation\Rules\File;
 
 class StorePostRequest extends FormRequest
@@ -31,10 +32,21 @@ class StorePostRequest extends FormRequest
     {
         return [
             'body' => ['nullable', 'string'],
-            'attachments' => 'array|max:50',
+            'attachments' => [
+                'array',
+                'max:50',
+                function ($attribute, $value, $fail) {
+                    // Custom rule to check the total size of all files
+                    $totalSize = collect($value)->sum(fn(UploadedFile $file) => $file->getSize());
+
+                    if ($totalSize > 1 * 1024 * 1024 * 1024) {
+                        $fail('The total size of all files must not exceed 1GB.');
+                    }
+                },
+            ],
             'attachments.*' => [
                 'file',
-                File::types(self::$extensions)->max(500 * 1024 * 1024)
+                File::types(self::$extensions),
             ],
             'user_id' => ['numeric']
         ];
@@ -52,7 +64,8 @@ class StorePostRequest extends FormRequest
     public function messages()
     {
         return [
-            'attachments.*' => 'Invalid file'
+            'attachments.*.file' => 'Each attachment must be a file.',
+            'attachments.*.mimes' => 'Invalid file type for attachments.',
         ];
     }
 }
--- resources/js/Components/app/PostModal.vue
+++ resources/js/Components/app/PostModal.vue
@@ -36,7 +36,7 @@ const attachmentExtensions = usePage().props.attachmentExtensions;
  */
 const attachmentFiles = ref([])
 const attachmentErrors = ref([])
-const showExtensionsText = ref(false)
+const formErrors = ref({});
 const form = useForm({
     body: '',
     attachments: [],
@@ -51,6 +51,18 @@ const show = computed({
 
 const computedAttachments = computed(() => {
     return [...attachmentFiles.value, ...(props.post.attachments || [])]
+})
+const showExtensionsText = computed(() => {
+    for (let myFile of attachmentFiles.value) {
+        const file = myFile.file
+        let parts = file.name.split('.')
+        let ext = parts.pop().toLowerCase()
+        if (!attachmentExtensions.includes(ext)) {
+            return true
+        }
+    }
+
+    return false;
 })
 
 const emit = defineEmits(['update:modelValue', 'hide'])
@@ -68,8 +80,8 @@ function closeModal() {
 
 function resetModal() {
     form.reset()
+    formErrors.value = {}
     attachmentFiles.value = []
-    showExtensionsText.value = false;
     attachmentErrors.value = [];
     if (props.post.attachments) {
         props.post.attachments.forEach(file => file.deleted = false)
@@ -104,6 +116,7 @@ function submit() {
 }
 
 function processErrors(errors) {
+    formErrors.value = errors
     for (const key in errors) {
         if (key.includes('.')) {
             const [, index] = key.split('.')
@@ -113,13 +126,7 @@ function processErrors(errors) {
 }
 
 async function onAttachmentChoose($event) {
-    showExtensionsText.value = false;
     for (const file of $event.target.files) {
-        let parts = file.name.split('.')
-        let ext = parts.pop().toLowerCase()
-        if (!attachmentExtensions.includes(ext)) {
-            showExtensionsText.value = true;
-        }
         const myFile = {
             file,
             url: await readFile(file)
@@ -209,6 +216,10 @@ function undoDelete(myFile) {
                                     <div v-if="showExtensionsText" class="border-l-4 border-amber-500 py-2 px-3 bg-amber-100 mt-3 text-gray-800">
                                         Files must be one of the following extensions <br>
                                         <small>{{attachmentExtensions.join(', ')}}</small>
+                                    </div>
+
+                                    <div v-if="formErrors.attachments" class="border-l-4 border-red-500 py-2 px-3 bg-red-100 mt-3 text-gray-800">
+                                        {{formErrors.attachments}}
                                     </div>
 
                                     <div class="grid gap-3 my-3" :class="[
