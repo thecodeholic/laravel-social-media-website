85f934551fb8ea04c3bd819ec0f7fbb12abdc160
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2023-12-08 23:41:59 +0400

    Set fixed height on comments section

--- resources/js/Components/app/PostItem.vue
+++ resources/js/Components/app/PostItem.vue
@@ -82,7 +82,7 @@ function sendReaction() {
                 </DisclosureButton>
             </div>
 
-            <DisclosurePanel class="mt-3">
+            <DisclosurePanel class="comment-list mt-3 h-[400px] overflow-auto">
                 <CommentList :post="post" :data="{comments: post.comments}"/>
             </DisclosurePanel>
         </Disclosure>
