512a0fcfc674702d45600bd0772a8c8666a7ddc4
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2023-12-09 12:21:41 +0400

    Give proper position to new group button

--- resources/js/Components/app/GroupList.vue
+++ resources/js/Components/app/GroupList.vue
@@ -1,10 +1,6 @@
 <script setup>
 import {Disclosure, DisclosureButton, DisclosurePanel} from "@headlessui/vue";
 import GroupListItems from "@/Components/app/GroupListItems.vue";
-import GroupModal from "@/Components/app/GroupModal.vue";
-import {ref} from "vue";
-
-const showNewGroupModal = ref(false)
 
 </script>
 
@@ -23,10 +19,6 @@ const showNewGroupModal = ref(false)
                     </div>
                 </DisclosureButton>
                 <DisclosurePanel>
-                    <button @click="showNewGroupModal = true"
-                            class="text-sm bg-indigo-500 hover:bg-indigo-600 text-white rounded py-1 px-2">
-                        new group
-                    </button>
                     <GroupListItems/>
                 </DisclosurePanel>
             </Disclosure>
@@ -34,15 +26,10 @@ const showNewGroupModal = ref(false)
         <div class="h-full overflow-hidden flex-col hidden lg:flex">
             <div class="flex justify-between">
                 <h2 class="text-xl font-bold">My Groups</h2>
-                <button @click="showNewGroupModal = true"
-                        class="text-sm bg-indigo-500 hover:bg-indigo-600 text-white rounded py-1 px-2">
-                    new group
-                </button>
             </div>
             <GroupListItems/>
         </div>
     </div>
-    <GroupModal v-model="showNewGroupModal"/>
 </template>
 
 <style scoped>
--- resources/js/Components/app/GroupListItems.vue
+++ resources/js/Components/app/GroupListItems.vue
@@ -2,12 +2,20 @@
 import TextInput from "@/Components/TextInput.vue";
 import GroupItem from "@/Components/app/GroupItem.vue";
 import {ref} from "vue";
+import GroupModal from "@/Components/app/GroupModal.vue";
 
 const searchKeyword = ref('')
+const showNewGroupModal = ref(false)
 </script>
 
 <template>
-    <TextInput :model-value="searchKeyword" placeholder="Type to search" class="w-full mt-4"/>
+    <div class="flex gap-2  mt-4">
+        <TextInput :model-value="searchKeyword" placeholder="Type to search" class="w-full"/>
+        <button @click="showNewGroupModal = true"
+                class="text-sm bg-indigo-500 hover:bg-indigo-600 text-white rounded py-1 px-2 w-[120px]">
+            new group
+        </button>
+    </div>
     <div class="mt-3 h-[200px] lg:flex-1 overflow-auto">
         <div v-if="false" class="text-gray-400 text-center p-3">
             You are not joined to any groups
@@ -117,6 +125,8 @@ const searchKeyword = ref('')
                        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
         </div>
     </div>
+
+    <GroupModal v-model="showNewGroupModal"/>
 </template>
 
 <style scoped>
