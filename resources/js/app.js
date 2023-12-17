da052c856e530ba2e3abd1d25140818bff1651db
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2023-12-17 15:10:15 +0400

    Implement switch to dark/light mode and saved the mode in localStorage

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,14 @@ import {ZiggyVue} from '../../vendor/tightenco/ziggy/dist/vue.m';
 import CKEditor from '@ckeditor/ckeditor5-vue';
 import '../css/app.css';
 
+const html = window.document.documentElement
+const darkMode = parseInt(localStorage.getItem('darkMode') || 1)
+if (darkMode) {
+    html.classList.add('dark')
+} else {
+    html.classList.remove('dark')
+}
+
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
